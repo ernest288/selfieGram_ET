597d0c49fe9097ddbf41e06119b8a2cf7710b9f5
Ernest288 <ernest.thu@gmail.com>
2017-11-02 19:29:40 -0400

    Initial Commit

---
+++ selfieGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfieGram
+//
+//  Created by Ernest Thu on 2017-11-02.
+//  Copyright © 2017 Ernest Thu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
